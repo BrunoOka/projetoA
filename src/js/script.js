e05907ac5cc36000738dd5b1e7c5c1290079b49b
brunoOka <brunohideki31@gmail.com>
2025-06-06 22:51:24 -0300

    ajustes (arrumando js)

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,12 @@
-alert("SEJA BEM VINDO AO GAME")
+window.onload = function () {
+    alert("SEJA BEM VINDO AO GAME");
 
-let p1 = prompt("Digite seu Nome");
-let p2 = prompt("Digite um PODER que gostaria de ter");
-let p3 = prompt("Digiteo nome de um VILÃO");
-let p4 = prompt("Digite um LUGAR que gostaria de estar");
+    let p1 = prompt("Digite seu Nome");
+    let p2 = prompt("Digite um PODER que gostaria de ter");
+    let p3 = prompt("Digite o nome de um VILÃO");
+    let p4 = prompt("Digite um LUGAR que gostaria de estar");
 
-let msg=document.getElementById('msg');
+    let msg = document.getElementById('msg');
 
-msg.innerHTML = `<p>Olá ${p1} o seu poder é ${p2} você vai enfrentar o(a) ${p3} em/no ${p4}</p>`
+    msg.innerHTML = `<p>Olá ${p1}, o seu poder é <strong>${p2}</strong>, e você vai enfrentar o(a) <strong>${p3}</strong> em/no <strong>${p4}</strong>.</p>`;
+}
